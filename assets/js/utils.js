237e6ee84998030ee66b5c5b18f442d66113887d
Kamsi Kodi <kamsikodi@gmail.com>
2024-05-22 12:43:48 +0100

    style: Added cash money headlines

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,5 +1,6 @@
 const cardChartIcons = document.querySelectorAll('.overview__cards-item-details-icon');
 const closeChartIcons = document.querySelectorAll('.close-chart');
+const cashMoneyItems = document.querySelectorAll('.transactions__wrapper-cash-money-item');
 
 cardChartIcons.forEach(cardChartIcon => {
     cardChartIcon.addEventListener('click', () => {
@@ -13,5 +14,12 @@ closeChartIcons.forEach(closeChartIcon => {
     closeChartIcon.addEventListener('click', () => {
         const chartItem = closeChartIcon.parentElement;
         chartItem.classList.remove('show-chart');
+    });
+});
+
+cashMoneyItems.forEach((cashMoneyItem, index) => {
+    cashMoneyItem.addEventListener('click', () => {
+        (index === 0) ? cashMoneyItems[1].classList.remove('active') : cashMoneyItems[0].classList.remove('active');
+        cashMoneyItem.classList.add('active');
     });
 });
